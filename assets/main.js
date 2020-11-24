ff8f0e9b7f755543e47d6f9843e8072fe4c90d7b
Ale <alessandrochb@gmail.com>
2020-11-24 15:27:40 +0100

    stampato in pagina una card per ogni film

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,25 @@
 var app = new Vue ({
     el:'#root',
     data: {
-
+        searchMovies: '',
+        allMovies: [],
+        isLoading: false,
     },
-    method: {
-
+    methods: {
+        getMovies(){
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params:{
+                    api_key: '5a579e747faf09425e97ffffa6a21111',
+                    query: this.searchMovies,
+                }
+            })
+            .then((element) =>{
+                console.log(element);
+                this.allMovies = element.data.results;
+                console.log(this.allMovies);
+                this.isLoading = true;
+            })
+        },
     },
     mounted(){
         console.log('hello');
