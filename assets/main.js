878e56cc1b9cd895d9897aadddcedc30780c1070
Ale <alessandrochb@gmail.com>
2020-11-25 17:20:29 +0100

    creata unica funzione getMovie per fare chiamate sia film che serie

--- assets/main.js
+++ assets/main.js
@@ -3,8 +3,11 @@ var app = new Vue ({
     data: {
         searchMovies: '',
         searchResoluts: '',
+        movies: [],
+        series: [],
         allMovies: [],
         isLoad: false,
+        isWaiting: false,
         numberStars: 5, //numero stelline voto
         languages: [
             'en',
@@ -15,72 +18,81 @@ var app = new Vue ({
             'ja',
             'pt'
         ],
-        isWaiting: false,
     },
     methods: {
-// FUNZIONE PER FARE CHIAMATA AJAX
+// FUNZIONE PER FARE CHIAMATA AJAX DEI FILM
+
         getMovies(){
-
-
-            if (this.searchMovies.trim()) {
+            if (this.searchMovies.trim())  {
                 this.isWaiting = true;
-                axios.get('https://api.themoviedb.org/3/search/movie', {
-                    params: {
-                        api_key: '5a579e747faf09425e97ffffa6a21111',
-                        query: this.searchMovies,
-                        language: 'it',
-                    },
-                })
-                .then((element) =>{
-                    console.log(element);
-                    this.allMovies = element.data.results;
+
+                axios.all([
+                    axios.get('https://api.themoviedb.org/3/search/movie', {
+                        params: {
+                            api_key: '5a579e747faf09425e97ffffa6a21111',
+                            query: this.searchMovies,
+                            language: 'it',
+                        },
+                    }),
+                    axios.get('https://api.themoviedb.org/3/search/tv', {
+                        params: {
+                            api_key: '5a579e747faf09425e97ffffa6a21111',
+                            query: this.searchMovies,
+                            language: 'it',
+                        },
+                    })
+                ])
+                .then(axios.spread((prima, seconda) => {
+                    console.log(prima.data);
+                    console.log(seconda.data);
+
+                    this.movies = prima.data.results;
+                    this.series = seconda.data.results;
+
+                    console.log(this.movies);
+                    console.log(this.series);
+
+                    this.allMovies = [...this.series, ...this.movies]; //new
                     console.log(this.allMovies);
+
                     this.isLoad = true;
 
                     this.searchResoluts = this.searchMovies;
 
                     this.searchMovies = '';
 
-                    this.allMovies.forEach((item) => {
+                    this.movies.forEach((item) => {
                         item.vote_average = Math.ceil(item.vote_average / 2);
                     });
 
                     this.isWaiting = false;
-                })
-
+                })) // fine then
+            }
 
             }
-        },
-        // getSeries(){
-        //     if (this.searchMovies.trim()) {
-        //         axios.get('https://api.themoviedb.org/3/search/tv', {
-        //             params: {
-        //                 api_key: '5a579e747faf09425e97ffffa6a21111',
-        //                 query: this.searchMovies,
-        //                 language: 'it',
-        //             }
-        //         })
-        //
-        //         .then((element) =>{
-        //             console.log(element);
-        //             this.allMovies = element.data.results;
-        //             console.log(this.allMovies);
-        //             this.isLoad = true;
-        //
-        //             this.searchMovies = '';
-        //
-        //
-        //             this.allMovies.forEach((item) => {
-        //                 item.vote_average = Math.ceil(item.vote_average / 2);
-        //                 // console.log(item.vote_average);
-        //
-        //             });
-        //         })
-        //     }
-        // },
-
     },
     mounted(){
         console.log('hello');
+
     }
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**/
