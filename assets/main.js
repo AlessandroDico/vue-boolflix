129c394978a20f88dab0c70b82214ebd8249871c
Ale <alessandrochb@gmail.com>
2020-11-25 14:17:49 +0100

    change logic stamp flegs (con v-for ripeteva la stampa lang-origin)

--- assets/main.js
+++ assets/main.js
@@ -5,34 +5,36 @@ var app = new Vue ({
         allMovies: [],
         isLoading: false,
         numberStars: 5,
-        images: [
+        languages: [
+            'en',
+            'de',
+            'it',
+            'es',
+            'fr',
+            'ja',
+            'pt'
+        ],
+        flags: [
             {
-                lang:'en',
-                flag: 'images/uk.jpg',
+                flag: 'images/en.jpg',
             },
             {
-                lang:'de',
-                flag: 'images/germany.jpg',
+                flag: 'images/de.jpg',
             },
             {
-                lang:'it',
-                flag: 'images/italy.jpg',
+                flag: 'images/it.jpg',
             },
             {
-                lang:'es',
-                flag: 'images/spain.jpg',
+                flag: 'images/es.jpg',
             },
             {
-                lang:'fr',
-                flag: 'images/france.jpg',
+                flag: 'images/fr.jpg',
             },
             {
-                lang:'ja',
-                flag: 'images/japan.jpg',
+                flag: 'images/ja.jpg',
             },
             {
-                lang:'pt',
-                flag: 'images/portugal.jpg',
+                flag: 'images/pt.jpg',
             },
         ]
     },
@@ -51,35 +53,39 @@ var app = new Vue ({
                 console.log(this.allMovies);
                 this.isLoading = true;
 
-                this.allMovies.forEach((item) => {
-                item.vote_average = Math.round(item.vote_average / 2);
-                // console.log(item.vote_average);
-
-                });
-            })
-        },
-        getSeries(){
-            axios.get('https://api.themoviedb.org/3/search/tv', {
-                params: {
-                    api_key: '5a579e747faf09425e97ffffa6a21111',
-                    query: this.searchMovies,
-                    language: 'it',
-                }
-            })
-
-            .then((element) =>{
-                console.log(element);
-                this.allMovies = element.data.results;
-                console.log(this.allMovies);
-                this.isLoading = true;
+                this.searchMovies = '';
 
                 this.allMovies.forEach((item) => {
                 item.vote_average = Math.round(item.vote_average / 2);
-                // console.log(item.vote_average);
-
                 });
+
             })
         },
+        // getSeries(){
+        //     axios.get('https://api.themoviedb.org/3/search/tv', {
+        //         params: {
+        //             api_key: '5a579e747faf09425e97ffffa6a21111',
+        //             query: this.searchMovies,
+        //             language: 'it',
+        //         }
+        //     })
+        //
+        //     .then((element) =>{
+        //         console.log(element);
+        //         this.allMovies = element.data.results;
+        //         console.log(this.allMovies);
+        //         this.isLoading = true;
+        //
+        //         this.searchMovies = '';
+        //
+        //
+        //         this.allMovies.forEach((item) => {
+        //         item.vote_average = Math.round(item.vote_average / 2);
+        //         // console.log(item.vote_average);
+        //
+        //         });
+        //     })
+        // },
 
     },
     mounted(){
